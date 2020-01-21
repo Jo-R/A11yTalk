35fb74f8886b046c0abeb1f32d523006af02a033
jo <jo.rigg@optix.co.uk>
2020-01-21 14:57:36 +0000

    add native select to demo page

--- a11y-demo/src/App.js
+++ a11y-demo/src/App.js
@@ -6,12 +6,20 @@ import { options } from "./options";
 // For this one:
 // if main is a div get landmark fails on axe
 // want save button to announce
+// compare with native cf how much code to get functionality??
 function App() {
   return (
     <main>  
       <h1>A simple page</h1>
       <Select options={options} />
       <button>Submit</button>
+      <select>
+        {
+          options.map((i) => 
+            <option value={i}>{i}</option>
+          )
+        }
+       </select>
     </main>
   );
 }
